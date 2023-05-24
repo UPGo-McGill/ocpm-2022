bf84287a2540d915db312874c52c0160d41f1619
Mikael Brunila <mikael.brunila@gmail.com>
2023-05-24 12:45:29 -0400

    stash commented lines etc

--- py/parse.py
+++ py/parse.py
@@ -1,6 +1,9 @@
 import PyPDF2
 import os
+import pandas as pd
+import pdftotext
 import re
+
 
 def read_letter_text(file_path):
     """
@@ -26,6 +29,43 @@ def read_letter_text(file_path):
         # Return the text content as a string
         return text_content
 
+def read_hearing_text(file_path):
+  with open(REPORT_PATH, "rb") as f:
+      pdf = pdftotext.PDF(f)
+
+  # All pages
+  pages = []
+  for idx, page in enumerate(pdf):
+    #if idx < 2:
+    #  print(page)
+    if idx > 2:
+      page = re.sub(r'^\s*([0-9]+\s*)+$','\n',page, flags=re.M)
+      page = re.sub(r'^Séance de la soirée.*\n?', '', page, flags=re.MULTILINE)
+      page = re.sub(r'^Mackay Morin Maynard et associés.*\n?', '', page, flags=re.MULTILINE)
+      page = page.split('FIN DE LA SOIRÉE')[0]
+      page = page.split('Je, soussignée')[0]
+      page = page.replace('Mme','MME')
+      pages.append(page)
+  pages = " ".join(pages)
+
+  # ALL OF THESE SEMI-WORK
+  #pattern = r"([A-Z'-’. ][A-Z'-’. ]* :\n)(.*?)(?=\n[A-Z'-’. ][A-Z'-’. ]* :|$)"
+  #pattern = r"([A-Z'-’. ][A-Z'-’. ]* ?\:)(.*?)(?=[A-Z'-’. ][A-Z'-’. ]* ?\:|$)"
+  #pattern = r"([A-Z'-’. ][A-Z'-’. ]* ?\:)\s*?(.*?)(?=[A-Z'-’. ][A-Z'-’. ]* ?\:|$)"
+  #pattern = r"([A-Z][A-Z'-’. ]* ?\:)\s*?(.*?)(?=[A-Z][A-Z'-’. ]* ?\:|$)"
+  pattern = r"([A-Z][A-Z.][A-Z'-’. ]* ?\:)(?:\s*(.*?))(?=[A-Z][A-Z'-’. ]* ?\:|$)"
+  #pattern = r"(MME|LA|M\.)\s+\w+\s+\w+:(.*?)(?=(MME|LA|M\.|$))(?<!:\s)"
+  result = re.findall(pattern, pages, re.DOTALL) # Find all regex matches in the 
+  
+  #print(result)
+  result = [(r[0].strip().replace("\n", ""), 
+             r[1].strip().replace("\n", "")) 
+            for r in result] 
+  # Create a list of tuples with the delimiter and following text
+
+  result = [(r[0].replace(":", "").strip(), r[1]) for r in result]
+  return result
+
 DATA_PATH = 'data/pdf_examples'
 
 letter_file = '8.2_marielle_ouellette.pdf'
@@ -36,58 +76,13 @@ LETTER_PATH = f'{DATA_PATH}/{letter_file}'
 
 report_file = '7.1_ocpm_16_05_12_franciscains.pdf'
 REPORT_PATH = f'{DATA_PATH}/{report_file}'
-
-import pdftotext
-
-with open(REPORT_PATH, "rb") as f:
-    pdf = pdftotext.PDF(f)
-
-# All pages
-pages = []
-for idx, page in enumerate(pdf):
-    #print(idx)
-    #print(text)
-    page = re.sub(r'^\s*([0-9]+\s*)+$','\n',page, flags=re.M)
-    page = re.sub(r'^Séance de la soirée.*\n?', '', page, flags=re.MULTILINE)
-    page = re.sub(r'^Mackay Morin Maynard et associés.*\n?', '', page, flags=re.MULTILINE)
-    page = page.split('FIN DE LA SOIRÉE')[0]
-    page = page.split('Je, soussignée')[0]
-    #page = os.linesep.join([s for s in page.splitlines() if s])
-    #page = page.replace("\n", " ")
-    #page = page.rstrip()
-    #page = page.lstrip()
-    #pages += f" {page} \n"
-    pages.append(page)
-#rx = r"([A-Z'-’. ][A-Z'-’. ]* :\n)"
-#pattern = r"([A-Z'-’. ][A-Z'-’. ]* :\n)"
-pages = " ".join(pages)
-#print(pages)
-#print(pages)
-
-#pattern = r"([A-Z'-’. ][A-Z'-’. ]* :\n)(.*?)(?=\n[A-Z'-’. ][A-Z'-’. ]* :|$)"
-#pattern = r"([A-Z'-’. ][A-Z'-’. ]* ?\:)(.*?)(?=[A-Z'-’. ][A-Z'-’. ]* ?\:|$)"
-#pattern = r"([A-Z'-’. ][A-Z'-’. ]* ?\:)\s*?(.*?)(?=[A-Z'-’. ][A-Z'-’. ]* ?\:|$)"
-#pattern = r"([A-Z][A-Z'-’. ]* ?\:)\s*?(.*?)(?=[A-Z][A-Z'-’. ]* ?\:|$)"
-pattern = r"([A-Z][A-Z'-’. ]* ?\:)(?:\s*(.*?))(?=[A-Z][A-Z'-’. ]* ?\:|$)"
-
-
-# Find all regex matches in the string
-matches = re.findall(pattern, pages, re.DOTALL)
-
-# Create a list of tuples with the delimiter and following text
-result = [(match[0], match[1]) for match in matches]
-
-# Print the resulting list of tuples
-#print(result)
-
-# THIS KIND OF WORKED
-#splitted_str = re.split(pattern, pages)
-#pairs = zip(splitted_str[::2], splitted_str[1::2])
-#result = [(delimiter, text) for delimiter, text in pairs]
-
-for r in result:
-  print(r)
+hearing_text = read_hearing_text(REPORT_PATH)
+for ht in hearing_text:
+  print(ht)
   print("\n")
 
+df = pd.DataFrame(hearing_text, columns=['speaker', 'text'])
+df.to_csv("data/text_report.csv")
+#print(df[:4])
 #report_text = read_letter_text(REPORT_PATH)
 #print(report_text)
