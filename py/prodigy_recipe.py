ce7889958a2f24c9d3f8d337a97e7cc4a50cf5f8
Mikael Brunila <mikael.brunila@gmail.com>
2024-07-26 17:29:06 -0400

    custom prodigy recipe

---
+++ py/prodigy_recipe.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+from prodigy.core import Arg, recipe
+from prodigy.components.preprocess import add_tokens
+from prodigy.components.stream import get_stream
+
+import prodigy
+import spacy
+
+@prodigy.recipe(
+    "sentiment",
+    dataset=Arg(help="Dataset to save answers to."),
+    file_path=Arg(help="Path to texts")
+)
+def sentiment(dataset: str, file_path: Path, lang:str="en"):
+    """Annotate the sentiment of texts using different mood options."""
+    blocks = [
+        {"view_id": "ner_manual"},
+        {"view_id": "choice", "text":None},
+        {"view_id": "text_input", "field_rows": 1, "field_label": "Notes"}
+    ]
+    
+    nlp = spacy.blank(lang) 
+    
+    stream = get_stream(file_path) # load in the JSONL file
+    stream.apply(add_tokens, nlp=nlp, stream=stream)  # tokenize the stream for ner_manual
+    stream = add_options(stream)   # add options to each task
+    
+    return {
+        "dataset": dataset,   # save annotations in this dataset
+        "view_id": "blocks",  # use the choice interface
+        "stream": stream,
+        "config":{"blocks": blocks, "labels": ["IRRELEVANT"]}
+    }
+
+def add_options(stream):
+    # Helper function to add options to every task in a stream
+    
+    options = [
+        {"id": "happy", "text": "green"},
+        {"id": "sad", "text": "grey"},
+        {"id": "angry", "text": "climate"},
+    ]
+    for task in stream:
+        task["options"] = options
+        yield task
