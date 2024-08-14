a470266fe8d046a0eaf424e8380798b1c8c20ea6
maybemkl <mikael.brunila@gmail.com>
2024-08-14 17:34:05 +0000

    count source files

---
+++ count_data_files.sh
@@ -0,0 +1,3 @@
+printf "Hearings: " && find data/all_hearings/ -type f | wc -l
+printf "Letters: " && find data/all_submissions/ -type f | wc -l
+printf "Reports: " && find data/all_reports/ -type f | wc -l
