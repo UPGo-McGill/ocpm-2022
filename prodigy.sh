04eecd1578a0da3420083477ba378f2886becfff
maybemkl <mikael.brunila@gmail.com>
2024-09-10 17:54:47 +0000

    correct db and class names

--- prodigy.sh
+++ prodigy.sh
@@ -1,1 +1,1 @@
-PRODIGY_PORT=8080 python -m prodigy sentiment test output/test100.jsonl -F py/prodigy_recipe.py
+PRODIGY_PORT=8080 python -m prodigy green_grey_climate round_1 output/for_prodigy/first_batch_N-17004.jsonl -F py/prodigy_recipe.py
